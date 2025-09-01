75c11fe87ddda40d97634dfc16f8e863920670f6
Aloka Senevirathne <ansenevirathne2003@gmail.com>
2025-09-01 13:31:15 +0530

    update

--- frontend/src/eventExpenses/EventManagement.js
+++ frontend/src/eventExpenses/EventManagement.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./EventManagement.css";
 
+//comment
 function EventManagement() {
   const [events, setEvents] = useState([]);
   const [showForm, setShowForm] = useState(false);
